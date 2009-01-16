81c176d5f8774c5c12dd4f49cb04f920e4f3812f
valda <valda@d0d07461-0603-4401-acd4-de1884942a52>
2009-01-16 08:11:57 +0000

    yourfilehost のテストを追加
    git-svn-id: http://svn.coderepos.org/share/lang/ruby/video_scraper@28503 d0d07461-0603-4401-acd4-de1884942a52

---
+++ test/www/video_scraper/test_your_file_host.rb
@@ -0,0 +1,28 @@
+# -*- mode:ruby; coding:utf-8 -*-
+
+require 'test/unit'
+$:.unshift(File.expand_path(File.dirname(__FILE__) + '/../../../lib'))
+require 'www/video_scraper'
+require 'filecache'
+require 'fileutils'
+
+class TestYourFileHost < Test::Unit::TestCase
+  def setup
+    @cache_root = '/tmp/test_video_scraper_cache'
+    WWW::VideoScraper.configure do |conf|
+      conf[:cache] = FileCache.new('TestVideoScraper', @cache_root, 60*60*24)
+    end
+  end
+
+  def teardown
+    # FileUtils.remove_entry_secure(@cache_root, true)
+  end
+
+  def test_scrape
+    vs = WWW::VideoScraper.scrape('http://www.yourfilehost.com/media.php?cat=video&file=XV436__03.wmv')
+    assert_equal 'http://www.yourfilehost.com/media.php?cat=video&file=XV436__03.wmv', vs.page_url
+    assert_match %r|http://cdn\w*\.yourfilehost\.com/unit1/flash8/\d+/[[:alnum:]]{32}\.flv|, vs.video_url
+    assert_match %r|http://cdn\w*\.yourfilehost\.com/thumbs/\d+/[[:alnum:]]{32}\.jpg|, vs.thumb_url
+    assert_equal 'XV436__03.wmv', vs.title
+  end
+end
