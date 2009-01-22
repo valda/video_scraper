6ef2f37bbfa0c9f60aadb374897fbeeb76463ea8
valda <valda@d0d07461-0603-4401-acd4-de1884942a52>
2009-01-22 09:39:07 +0000

    tube8 で 3gp の URL も取得するようにした
    git-svn-id: http://svn.coderepos.org/share/lang/ruby/video_scraper@28852 d0d07461-0603-4401-acd4-de1884942a52

--- lib/www/video_scraper/tube8.rb
+++ lib/www/video_scraper/tube8.rb
@@ -5,6 +5,7 @@ require File.expand_path(File.dirname(__FILE__) + '/base')
 module WWW
   module VideoScraper
     class Tube8 < Base
+      attr_reader :video_url_3gp
       url_regex %r!\Ahttp://www\.tube8\.com/.*/(\d+)(?:/|$)!
 
       private
@@ -17,6 +18,14 @@ module WWW
         uri = URI.parse(@page_url)
         @thumb_url = URI.join("#{uri.scheme}://#{uri.host}", flashvars['imageUrl'][0]).to_s
         @title = doc.at('//h1[@class="text"]').inner_html rescue nil
+        doc.search('//a').each do |elem|
+          if href = elem.attributes['href']
+            if href.match(/\.3gp$/)
+              @video_url_3gp = href
+              break
+            end
+          end
+        end
       end
     end
   end
--- test/www/video_scraper/test_tube8.rb
+++ test/www/video_scraper/test_tube8.rb
@@ -18,8 +18,9 @@ class TestTube8 < Test::Unit::TestCase
   def test_scrape
     vs = WWW::VideoScraper.scrape('http://www.tube8.com/anal/alexis-amore-pov/56983/')
     assert_equal 'http://www.tube8.com/anal/alexis-amore-pov/56983/', vs.page_url
-    assert_match %r|http://medianl\d+\.tube8\.com/flv/[[:alnum:]]{32}/[[:alnum:]]{8}/\d{4}/\d{2}/[[:alnum:]]+/[[:alnum:]]+\.flv|, vs.video_url
+    assert_match %r|http://media\w+\.tube8\.com/flv/[[:alnum:]]{32}/[[:alnum:]]{8}/\d{4}/\d{2}/[[:alnum:]]+/[[:alnum:]]+\.flv|, vs.video_url
     assert_equal 'http://www.tube8.com/vs/83/56983.jpg', vs.thumb_url
     assert_equal 'Alexis Amore POV', vs.title
+    assert_match %r!http://mobile\d+\.tube8\.com/flv/[[:alnum:]]{32}/[[:alnum:]]{8}/\d{4}/\d{2}/[[:alnum:]]+/[[:alnum:]]+\.3gp!, vs.video_url_3gp
   end
 end
