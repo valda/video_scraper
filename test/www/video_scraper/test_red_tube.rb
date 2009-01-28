d775b8ba35b26e684de19f091fd5ad31c4270b26
valda <valda@d0d07461-0603-4401-acd4-de1884942a52>
2009-01-28 07:57:49 +0000

    redtube で最近の動画を取得できていなかった問題を修正 redtube でサムネイルを取得
    git-svn-id: http://svn.coderepos.org/share/lang/ruby/video_scraper@29178 d0d07461-0603-4401-acd4-de1884942a52

--- test/www/video_scraper/test_red_tube.rb
+++ test/www/video_scraper/test_red_tube.rb
@@ -4,10 +4,10 @@ require File.dirname(__FILE__) + '/../../test_helper'
 
 class TestRedTube < Test::Unit::TestCase
   def test_scrape
-    vs = WWW::VideoScraper::RedTube.scrape('http://www.redtube.com/8415', default_opt)
-    assert_equal 'http://www.redtube.com/8415', vs.page_url
-    assert_match %r!http://dl\.redtube\.com/_videos_t4vn23s9jc5498tgj49icfj4678/0000008/Z2XDJA1ZL\.flv!, vs.video_url
-    assert_nil vs.thumb_url
+    vs = WWW::VideoScraper::RedTube.scrape('http://www.redtube.com/15021', default_opt)
+    assert_equal 'http://www.redtube.com/15021', vs.page_url
+    assert_equal 'http://dl.redtube.com/_videos_t4vn23s9jc5498tgj49icfj4678/0000015/X6KKXB0DB.flv', vs.video_url
+    assert_equal 'http://thumbs.redtube.com/_thumbs/0000015/0015021/0015021_001.jpg', vs.thumb_url
     assert_match %r!<object\s+.*</object>!, vs.embed_tag
   end
 end
