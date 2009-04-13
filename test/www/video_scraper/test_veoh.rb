b4042dc65e9a041d15ff7b31e7b5ae700eee2fd2
YAMAGUCHI Seiji <valda@underscore.jp>
2009-04-13 12:59:21 +0900

    veoh の URL のバリエーションを追加

--- test/www/video_scraper/test_veoh.rb
+++ test/www/video_scraper/test_veoh.rb
@@ -8,7 +8,7 @@ class TestVeoh < Test::Unit::TestCase
     assert_equal 'http://www.veoh.com/videos/v6245232rh8aGEM9', vs.page_url
     assert_match %r|http://content\.veoh\.com/flash/p/\d/[[:alnum:]]{16}/[[:alnum:]]{40}\.fll\?ct=[[:alnum:]]{48}|, vs.video_url
     assert_match %r|http://p-images\.veoh\.com/image\.out\?imageId=media-[[:alnum:]]+.jpg|, vs.thumb_url
-    assert_match %r|^<embed\s.*>$|, vs.embed_tag
+    assert_match %r|^<object\s.*>$|, vs.embed_tag
   end
 
   def test_canonical_url
@@ -18,5 +18,7 @@ class TestVeoh < Test::Unit::TestCase
     assert_equal 'http://www.veoh.com/videos/v16112008KGD7Pg2n', vs.page_url
     vs = WWW::VideoScraper::Veoh.scrape('http://www.veoh.com/videos/v19937773gwSJPMk?rank=0&jsonParams=%7B%22numResults%22%3A20%2C%22rlmin%22%3A0%2C%22query%22%3A%22Shaman+King+01%22%2C%22rlmax%22%3Anull%2C%22veohOnly%22%3Atrue%2C%22order%22%3A%22default%22%2C%22range%22%3A%22a%22%2C%22sId%22%3A%22192998624295114150%22%7D&searchId=192998624295114150&rank=1', default_opt)
     assert_equal 'http://www.veoh.com/videos/v19937773gwSJPMk', vs.page_url
+    vs = WWW::VideoScraper::Veoh.scrape('http://www.veoh.com/browse/videos/category/comedy/watch/v17078605sszQzbBF')
+    assert_equal 'http://www.veoh.com/videos/v17078605sszQzbBF', vs.page_url
   end
 end
